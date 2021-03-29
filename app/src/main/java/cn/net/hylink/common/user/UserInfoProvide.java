c0f20fcf942d4dce25f3668ee237033f5e9c1a6a
hsy1992 <553012786@qq.com>
2021-03-29 09:40:39 +0800

    no message

--- app/src/main/java/cn/net/hylink/common/user/UserInfoProvide.java
+++ app/src/main/java/cn/net/hylink/common/user/UserInfoProvide.java
@@ -59,6 +59,8 @@ public class UserInfoProvide {
     private void setUserInfo(Cursor cursor) {
         if (cursor != null) {
             userInfo = CursorToBeanUtil.cursor2Model(cursor, SignInResponseBean.ResultBean.ListBean.class);
+        } else {
+            userInfo = null;
         }
     }
 }
